e0e176f7e0a54ffe4284f67645bdc472bdc0308e
= <christopher_brisson@hotmail.com>
2019-10-02 00:29:21 -0300

    adding more robut methods to break down amounts

--- main.py
+++ main.py
@@ -21,6 +21,29 @@ def split_filenames_by_person(filenames):
       files_by_person[person_name] = [filename]
   return files_by_person
 
+# "Date","Description","Original Description","Amount","Transaction Type","Category","Account Name","Labels","Notes"
+# Date,Description,Amount
+# Date,Description,Category,Reference Number,Amount
+def create_person_expense_list(files):
+  start_date = datetime.strptime('08/01/2019', '%m/%d/%Y')
+  end_date = datetime.strptime('08/30/2019', '%m/%d/%Y')
+  debit_array = []
+  credit_array = []
+  for filename in files:
+    csv_reader = csv.DictReader(open(filename))
+    for row in csv_reader:
+      if 'Date' in row:
+        row_date = datetime.strptime(row['Date'], '%m/%d/%Y')
+        if row_date > start_date and row_date < end_date:
+          print(row)
+          amount = float(row['Amount'])
+          expense_row = {'Description': row['Description'],'Date': row['Date'], 'Amount': amount}
+          if amount < 0 or ('Transaction Type' in row and row['Transaction Type'] == 'debit'):
+              debit_array.append(expense_row)
+          else:
+            credit_array.append(expense_row)      
+  return debit_array, credit_array
+
 def totals_for_person(filenames):
   person_total_debit = 0
   person_total_credit = 0
@@ -45,12 +68,22 @@ def totals_for_person(filenames):
               person_total_debit += amount
   return person_total_credit, person_total_debit
 
+def sum_amounts(expense_list):
+  total_amount = 0
+  for row in expense_list:
+    total_amount += float(row['Amount'])
+  return total_amount
+
 def main():
   filenames = get_filenames("expense_files/")
   files_by_person = split_filenames_by_person(filenames)
+  person_expenses = {}
   for person_name, person_files in files_by_person.items():
+    person_expenses[person_name + ' debit'], person_expenses[person_name + ' credit'] = create_person_expense_list(person_files)
     credit_amt, debit_amt = totals_for_person(person_files)
-    print("%s's Info: Debit=%f Credit=%f" % (person_name, debit_amt, credit_amt))
+    # print("%s's Info: Debit=%f Credit=%f" % (person_name, debit_amt, credit_amt))
+    print("%s debit: %f" % (person_name, sum_amounts(person_expenses[person_name + ' debit'])))
+    print("%s credit: %f" % (person_name, sum_amounts(person_expenses[person_name + ' credit'])))
 
 if __name__ == "__main__":
   main()
